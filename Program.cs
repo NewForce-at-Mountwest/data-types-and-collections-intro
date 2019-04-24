7288d6ed71ea26e0c4ef96e5bd1e009d0343868d
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-04-24 14:55:18 -0400

    example code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// The using directives give us access to .NET's pre-defined types, like strings and integers 
+﻿// The using directives give us access to .NET's pre-defined types, like strings and integers
 using System;
 using System.Collections.Generic;
 
@@ -16,72 +16,225 @@ namespace intro
         static void Main(string[] args)
         {
 
-            // --------- DATA TYPES ----------- // 
-            // We're going to build a sentence about a grocery store
 
+            // --------- DATA TYPES ----------- //
+            // We're going to build a sentence about a grocery store
             // Declare a string variable to hold the name of a food item
+            // string foodItem = "Taco Salad";
 
             // Declare an integer variable to hold how many of them we have
+            // int numberOfTacoSalads = 3;
 
             // Declare a double variable to hold the price
+            // double priceOfTacoSalad = 3.50;
+
+            // Print the sentence to the console.
+            // Console.WriteLine($"We have {numberOfTacoSalads} {foodItem}s for ${priceOfTacoSalad}.");
+
+            // // 1. In the `Main` method of your `Program.cs` file, create variables to hold the following data:
+            // // - The name of the taco shop (a string)
+            // string name = "Jordan's Taco Shop";
+
+            // // - The address of the taco shop (a string)
+            // string address = "123 Sesame Street";
+            // // - The monthly operating budget (a double)
+            // double budget = 100.00;
+            // // - Whether the taco shop is currently open (a boolean)
+            // bool isOpen= false;
+            // // - The number of employees at the taco shop (an integer)
+            // int numberOfEmployees = 5;
+            // // - The number of items on the menu of the taco shop (an integer)
+            // int itemsOnMenu = 15;
+            // // 2. Create a sentence about the taco shop using at least three of the variables you just created.
+            // string tacoShopSentence = $"{name} is at {address} and has a budget of {budget}.";
+            // Console.WriteLine(tacoShopSentence);
+            // // 3. Print your sentence to your terminal.
+            // // 4. Example output: "Jordan's Awesome Taco Shop is located at 45 Taco Road and has 10 employees".
 
 
-            // Print the sentence to the console. 
 
-
-
-            // --------- CONDITIONALS ----------- // 
+            // --------- CONDITIONALS ----------- //
 
             // If/else conditions work a lot like JavaScript. Let's check to see if the price is more than $10.
-          
+            // int costOfTaco = 11;
 
-            // Fun fact: they also have ternary statements in C# 
-         
-            // --------- COLLECTIONS AND LOOPS ----------- // 
 
-           
+            // if(costOfTaco > 10){
+            //     Console.WriteLine("Too expensive!");
+            // } else if (costOfTaco >= 5 && costOfTaco <= 9){
+            //     Console.WriteLine("Just the right price!");
+            // } else {
+            //     Console.WriteLine("Too cheap");
+            // }
+
+            // Fun fact: they also have ternary statements in C#
+
+            // string howMuchIsTheTaco = costOfTaco > 10 ? "Too expensive!" : "So cheap let's buy it";
+
+            // Console.WriteLine(howMuchIsTheTaco);
+
+            // --------- COLLECTIONS AND LOOPS ----------- //
+
+
             // Define a list of strings. Each string should represent a food item at the grocery store
-            List<string> foodItems = new List<string>();
+            // List<string> foodItems = new List<string>(){
+            //     "Apples",
+            //     "Ketchup",
+            //     "Salt"
+            // };
 
-            // Print one food item by its index (just like arrays in JS!)
+            // foodItems.Add("Watermelon");
+            // foodItems.Add("Beef");
+            // foodItems.Add("Meatloaf");
 
-            // Print each food item to the console with a loop
 
-            // Add a conditional to the loop- if the item is "taco", print "HECK YEAH" to the console. If not, print "this would be better if it were a taco" to the console. 
-    
+
+            // foreach(string singleFood in foodItems){
+            //     if(singleFood == "Meatloaf"){
+            //         Console.WriteLine("Shred it");
+            //     } else {
+            //         Console.WriteLine("Do not shred it.");
+            //     }
+            //     // Console.WriteLine(singleFood);
+            // }
+
+            //  Lightning Exercise Three: Loopin'
+            // 1. Create a list of strings. Each string should represent an employee at the taco shop. Add your own name in there somewhere.
+            List<string> employees = new List<string>(){
+                "Charles",
+                "Matt",
+                "Nikki",
+                "Sable",
+                "Jordan"
+            };
+            // 2. Loop through the list of names and print each one to the console.
+            foreach (string singleEmployee in employees)
+            {
+                if (singleEmployee == "Jordan")
+                {
+                    Console.WriteLine($"{singleEmployee} is a very good employee");
+                }
+                else
+                {
+                    Console.WriteLine($"{singleEmployee} is also a good employee!");
+                }
+                // Console.WriteLine(singleEmployee);
+            }
+
+
 
             // ---------- LISTS AND LIST METHODS -------------------------//
-       
+
             // Remove something from the list
+            Console.WriteLine(employees.Count);
+            employees.Remove("Jordan");
+            Console.WriteLine(employees.Count);
 
             // Find something in the list
+            string employeeOfTheMonth = employees.Find(employee => employee == "Taco");
 
-            // Check to see if the list contains both "hot dogs" and "ketchup". If so, print "all set for our cookout!" to the console. If not, print "no cookout for you". 
+            Console.WriteLine(employeeOfTheMonth);
 
-            // Use .Count figure out how many items are in the store's inventory. 
 
-            // Print everything in our store's inventory to the console with a .ForEach loop 
+
+            // Use .Contains
+            if (employees.Contains("Benjamin"))
+            {
+                Console.WriteLine("This team has the employee of the month on it!");
+            }
+            else
+            {
+                Console.WriteLine("No employee of the month for you!");
+            }
+
+
+            // Print everything in our store's inventory to the console with a .ForEach loop
+            employees.ForEach(e => Console.WriteLine(e));
 
 
             // -------------- DICTIONARIES ---------------------//
 
             // Create a new dictionary to represent foods at the grocery store. This time, we'll keep track of the food item's name and price
+            // const groceryStore = {
+            //     "kiwi": 1.99,
+            //     "burrito": 4.99
+            // }
 
-            // Add something to the dictionary
+            Dictionary<string, double> groceryStore = new Dictionary<string, double>(){
+                {"Beef", 9.99},
+                {"Ketchup", 2.99}
+            };
 
-            // Iterate over the dictionary 
+            groceryStore.Add("kiwi", 1.99);
+            groceryStore.Add("burrito", 4.99);
 
-            // Use bracket notation to access something inside a dictionary by its key 
 
-            
+            Console.WriteLine(groceryStore["Beef"]);
+
+            foreach (KeyValuePair<string, double> groceryItem in groceryStore)
+            {
+                Console.WriteLine($"{groceryItem.Key} costs {groceryItem.Value}");
+            }
+
+
+            //  Lightning Exercise Five: Dictionaries
+
+            // 1. Create a dictionary to represent the menu at the taco shop. Each KeyValuePair in the dictionary should have a key of a string that represents the name of the menu item and a value that represents its price (double).
+              Dictionary<string, double> menuItems = new Dictionary<string, double>(){
+                {"Fish Tacos", 6.99},
+                {"Chicken Tacos", 4.99},
+                {"Beef Tacos", 8.99},
+                {"Shrimp Tacos", 7.99}
+            };
+            // 2. Create a dictionary to represent the employees at the taco shop. Each KeyValuePair should have a key of a string that represents the employee's name and a value of a string that represents their favorite menu item.
+               Dictionary<string, string> tacoShopEmployees = new Dictionary<string, string>(){
+                {"Haley", "Fish Tacos"},
+                {"James", "Chicken Tacos"},
+                {"Sally", "Beef Tacos"},
+                {"Kim", "Shrimp Tacos"}
+
+            };
+            // 3. Loop over the dictionary of employees. For each employee, print their favorite menu item and it's price. Example output: "Jenny's favorite dish is fish tacos for only $6.99"
+
+            foreach(KeyValuePair<string, string> singleEmployee in tacoShopEmployees){
+                string name = singleEmployee.Key;
+                string favoriteFood = singleEmployee.Value;
+                double price = menuItems[favoriteFood];
+                Console.WriteLine($"{name}'s favorite food is {favoriteFood} and it costs {price}.");
+            }
+
+
             // ----------------- SETS ----------------------//
 
-        // A hashset is a collection of unique items. You use a HashSet when you want to store an unordered collection of items. 
+            // A hashset is a collection of unique items. You use a HashSet when you want to store an unordered collection of items.
+            List<int> testScores = new List<int>(){
+                99,
+                99,
+                100,
+                60,
+                60,
+                60,
+                100
+            };
 
-        // Create a hashset to represent types of produce at the grocery store
+            HashSet<int> uniqueScores = new HashSet<int>();
 
-        // Try to add the same type of produce twice. It won't work!
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Here are allllll the scores:");
+            foreach(int score in testScores){
+                uniqueScores.Add(score);
+            }
+
+            Console.WriteLine("Here are the unique scores:");
+
+            foreach(int score in uniqueScores){
+                Console.WriteLine(score);
+            }
+
+            uniqueScores.Add(100);
+            // Create a hashset to represent types of produce at the grocery store
+
+            // Try to add the same type of produce twice. It won't work!
+
         }
     }
 }
--- lightning.md
+++ lightning.md
@@ -9,35 +9,35 @@ In your console application, we're going to keep track of some information about
 - Whether the taco shop is currently open (a boolean)
 - The number of employees at the taco shop (an integer)
 - The number of items on the menu of the taco shop (an integer)
-2. Create a sentence about the taco shop using at least three of the variables you just created. 
-3. Print your sentence to your terminal. 
+2. Create a sentence about the taco shop using at least three of the variables you just created.
+3. Print your sentence to your terminal.
 4. Example output: "Jordan's Awesome Taco Shop is located at 45 Taco Road and has 10 employees".
 
 
-Lightning Exercise Two: Conditionals 
-1. Use an if /else statement to check to see if the taco restaurant is open. If so, print "Let's gooooo" to the console. If not, print "gosh darn". 
-2. Write a conditional to check the number of employees at the taco shop. 
+Lightning Exercise Two: Conditionals
+1. Use an if /else statement to check to see if the taco restaurant is open. If so, print "Let's gooooo" to the console. If not, print "gosh darn".
+2. Write a conditional to check the number of employees at the taco shop.
 - If the number is between 0 and 4, print "This business is very tiny".
 - If the number is betwene 5 and 9, print "This business is still pretty small"
 - If the number is between 10 and 14, print "This is a small business"
 - If the number is between 15 and 19, print "This is a medium sized business"
-- If the number is over 20, print "THIS BUSINESS IS HUGE" 
+- If the number is over 20, print "THIS BUSINESS IS HUGE"
 
 
 Lightning Exercise Three: Loopin'
 1. Create a list of strings. Each string should represent an employee at the taco shop. Add your own name in there somewhere.
 2. Loop through the list of names and print each one to the console.
-3. Add a conditional to your loop that checks to see if the current name is your name. If so, print "[Your name here] is the best employee ever!!!" If not, print "[Someone else's name] is a so-so employee." 
+3. Add a conditional to your loop that checks to see if the current name is your name. If so, print "[Your name here] is the best employee ever!!!" If not, print "[Someone else's name] is a so-so employee."
 
 Lightning Exercise Four: Lists Methods
 
 1. Use .Add() to add another employee to your list of employees
 2. Use .Insert() to add a new employee to the end of the list.
-3. Create a new list of strings to represent the names of customers at the taco shop. 
+3. Create a new list of strings to represent the names of customers at the taco shop.
 4. Use .Find() to extract a customer named "Kim", if one exists, from your list of customers.
-5. Kim was a customer at the taco shop, but they've decided to hire her. Use .Remove() to remove her from the customers list and add her to the employees list. 
+5. Kim was a customer at the taco shop, but they've decided to hire her. Use .Remove() to remove her from the customers list and add her to the employees list.
 6. How many customers does the taco shop have?
-7. The taco shop would like to see a list of everyone who's in the store regularly, both customers and employees. Use .AddRange() to combine the list of customers and employees and then use a .ForEach() loop print the newly combined list to the console. 
+7. The taco shop would like to see a list of everyone who's in the store regularly, both customers and employees. Use .AddRange() to combine the list of customers and employees and then use a .ForEach() loop print the newly combined list to the console.
 
 
 Lightning Exercise Five: Dictionaries
@@ -46,7 +46,7 @@ Lightning Exercise Five: Dictionaries
 3. Loop over the dictionary of employees. For each employee, print their favorite menu item and it's price. Example output: "Jenny's favorite dish is fish tacos for only $6.99"
 
 Lightning Exercise Six: HashSets
-1. The taco shop has generated a report of their top selling menu items. Unfortunately, the report gives them a line item for every time they've sold that item, when they really just want a consolidated list of their top sellers. They don't care what order it's in. The following list represents the individual sales for their top selling menu items: 
+1. The taco shop has generated a report of their top selling menu items. Unfortunately, the report gives them a line item for every time they've sold that item, when they really just want a consolidated list of their top sellers. They don't care what order it's in. The following list represents the individual sales for their top selling menu items:
 
 ```C#
 
@@ -67,33 +67,13 @@ List<string> salesReport = new List<string>(){
 }
 ```
 2. Create a new HashSet called `topSellers`
-3. Loop over the `salesReport` list and add each item to `topSellers`. 
-4. Print each item in the `topSellers` HashSet to the console. 
+3. Loop over the `salesReport` list and add each item to `topSellers`.
+4. Print each item in the `topSellers` HashSet to the console.
 
 
 ```C#
-Dictionary<string, double> menuItems = new Dictionary<string, double>(){
-                {"Fish Tacos", 6.99},
-                {"Chicken Tacos", 4.99},
-                {"Beef Tacos", 8.99},
-                {"Shrimp Tacos", 7.99}
-            };
 
-            Dictionary<string, string> employees = new Dictionary<string, string>(){
-                {"Haley", "Fish Tacos"},
-                {"James", "Chicken Tacos"},
-                {"Sally", "Beef Tacos"},
-                {"Kim", "Shrimp Tacos"}
-
-            };
-
-            foreach(KeyValuePair<string, string> e in employees){
-                string name = e.Key; 
-                string favoriteFood = e.Value;
-                double price = menuItems[favoriteFood];
-                Console.WriteLine($"{name}'s favorite food is {favoriteFood} for only {price}");
-            }
 
 ```
-            
+
 
